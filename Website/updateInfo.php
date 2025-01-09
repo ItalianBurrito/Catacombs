8d529f126e2a44fecc0170b4ac34bf59c8c3f5bb
ItalianBurrito <eat_lead76@hotmail.com>
2025-01-09 00:37:01 -0800

    create the page to input info into database, started adding classifications and fields for items

---
+++ Website/updateInfo.php
@@ -0,0 +1,47 @@
+<!--This is the player view-->
+
+<!DOCTYPE html>
+
+<html lang ="en">
+  <head>
+    <link rel="manifest" href="manifest.json" />
+
+    <meta name="apple-mobile-web-app-status-bar" content="#db4938" />
+    <meta name="theme-color" content="#db4938" />
+
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/style.css" />
+    <title>Add Content</title>
+  </head>
+  <body>
+    <main>
+      <nav>
+      </nav>
+
+      <div id="refresh-bar" className="refresh_bar" display="">
+               <Button id="refresh_btn" key="install" aria-label="close" color="inherit"
+                       className="refresh_bar_class" onClick="refreshPWA()" border='0'>
+                   New Content available. Press here to refresh!
+               </Button>
+       </div>
+
+       <div id="databaseForm">
+         <input type ="" name="" class="databaseInput"><br>
+         <select id="itemType" name="itemType">
+           <option value=""></option>
+           <option value="clothing">Clothing</option>
+           <option value="weapon">Weapon</option>
+           <option value="magicWeapon">Magic Weapon</option>
+           <option value="armour">Armour</option>
+           <option value="magicArmour">Magic Armour</option>
+           <option value="wonderous">Wonderous Item</option>
+           <option value="other">Other</option>
+         <input type="text" name="itemName" class="databaseInput"><br>
+       </div>
+
+        <a href='home.php'>Back to campaign select</a>
+    </main>
+    <script src="js/app.js"></script>
+  </body>
+</html>
